657003e699d61d38801d745d2e0011cd6190a642
Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
2026-01-06 11:13:24 +0100

    Refactor binder.py to support notebook.link
    Closes https://github.com/repo2wasm/repo2wasm-py/issues/24

--- src/repo2wasm/binder.py
+++ src/repo2wasm/binder.py
@@ -7,39 +7,61 @@ from .buildpacks.install import InstallBuildPack
 
 logger = logging.getLogger(__name__)
 
-ALLOWED_BINDER_DIR = [".binder", "binder", "."]
+# As documented in https://repo2docker.readthedocs.io/en/latest/specification/
+ALLOWED_REPO2DOCKER_DIR = [".binder", "binder", "."]
 
-ALLOWED_BINDER_CONFIGURATION_MAPPING = {
+ALLOWED_REPO2DOCKER_CONFIGURATION_MAPPING = {
     "environment.yml": CondaBuildPack,
     "requirements.txt": RequirementsBuildPack,
     "install.R": InstallBuildPack,
 }
 
+# As documented in https://notebook.link/docs/user-guide/enable-your-github-repository#define-the-environment
+ALLOWED_NOTEBOOK_LINK_DIR = [".nblink", "."]
 
-def find_configuration_file(path_to_repository):
-    for configuration_file in ALLOWED_BINDER_CONFIGURATION_MAPPING:
-        for binder_dir in ALLOWED_BINDER_DIR:
-            file_path = os.path.join(path_to_repository, binder_dir, configuration_file)
-            logger.debug("Searching for %s ...", file_path)
-            if os.path.isfile(file_path):
-                logger.debug("Found %s", file_path)
-                return file_path
+ALLOWED_NOTEBOOK_LINK_CONFIGURATION_MAPPING = {
+    "environment.yml": CondaBuildPack,
+}
 
-    raise RuntimeError("Configuration file not found.")
+CONFIGURATION_PROVIDERS = {
+    "repo2docker": (ALLOWED_REPO2DOCKER_CONFIGURATION_MAPPING, ALLOWED_REPO2DOCKER_DIR),
+    "notebook.link": (
+        ALLOWED_NOTEBOOK_LINK_CONFIGURATION_MAPPING,
+        ALLOWED_NOTEBOOK_LINK_DIR,
+    ),
+}
 
 
 def get_buildpack(path_to_repository, ide, output_dir, forgiving=False):
-    configuration_file_path = find_configuration_file(path_to_repository)
+    configuration_file_path = None
+    BuildPack = None
 
-    configuration_file = os.path.basename(configuration_file_path)
+    for provider, (
+        configuration_mapping,
+        allowed_dir,
+    ) in CONFIGURATION_PROVIDERS.items():
+        logger.debug("Testing for %s as provider ...", provider)
+        for configuration_file in configuration_mapping:
+            for binder_dir in allowed_dir:
+                file_path = os.path.join(
+                    path_to_repository, binder_dir, configuration_file
+                )
+                logger.debug("Searching for %s ...", file_path)
+                if os.path.isfile(file_path):
+                    logger.debug("Found %s", file_path)
+                    configuration_file_path = file_path
+                    BuildPack = configuration_mapping[configuration_file]
+                    break
 
-    for (
-        buildpack_configuration_file,
-        BuildPack,
-    ) in ALLOWED_BINDER_CONFIGURATION_MAPPING.items():
-        if configuration_file == buildpack_configuration_file:
-            return BuildPack(
-                path_to_repository, configuration_file_path, ide, output_dir, forgiving
-            )
+            if configuration_file_path is not None:
+                break
 
-    raise RuntimeError("Build pack not found.")
+        if configuration_file_path is not None:
+            break
+
+    if configuration_file_path is None:
+        raise RuntimeError("Configuration file not found.")
+
+    return BuildPack(
+        path_to_repository, configuration_file_path, ide, output_dir, forgiving
+    )
